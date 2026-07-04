825bea3d5c211777b9d7a3a478d820fac411a042
AKERMI RIHEM <154096280+rihem-akermi@users.noreply.github.com>
2026-07-04 21:54:33 +0100

    check if the port is ok 3001 ?
    from
    npm run ...
    start:dev -> dev

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3001);//port different de next.js 3000
+  console.log('✅✅ Backend started on http://localhost:3001 ✅✅');
+
 }
+
 bootstrap();
